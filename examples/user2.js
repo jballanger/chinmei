781be23d67a7e066cc77822f939a8031225a834d
Julien BALLANGER <jballang@e1r3p17.42.fr>
2017-08-02 15:50:49 +0200

    user example 2

---
+++ examples/user2.js
@@ -0,0 +1,16 @@
+const Chinmei = require('chinmei');
+
+var myChinmei = new Chinmei('baduname', 'badpassw');
+/* Error: Failed to login on MyAnimeList (401) */
+
+myChinmei.setUser('uname', 'passw');
+myChinmei.getUser().then((user) => {
+	console.log(`Hello ${user.username} ! (${user.id})`);
+	/* Hello uname ! (19283746) */
+}).catch((e) => {
+	console.error(e);
+});
+
+myChinmei.on('error', (err) => {
+	console.error(err);
+});
