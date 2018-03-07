11ea9c967a4e7e5dbded441be6f1f151d058c536
Julien <ballanger.julien@gmail.com>
2018-03-07 18:13:26 +0100

    update getUser example

--- examples/user.js
+++ examples/user.js
@@ -1,13 +1,25 @@
 const Chinmei = require('../index.js');
-var myChinmei = new Chinmei(process.env.MAL_USER, process.env.MAL_PASS);
+
+const myChinmei = new Chinmei(process.env.MAL_USER, process.env.MAL_PASS);
+
+// Using async / await
+try {
+  const user = await myChinmei.getUser();
+  console.log(user);
+} catch (e) {
+  console.error(e);
+}
 
 myChinmei.getUser().then((user) => {
-	console.log(`Hello ${user.username} ! (${user.id})`);
-	/* Hello _julien ! (6338595) */
+  console.log(user);
 }).catch((e) => {
-	console.error(e);
+  console.error(e);
 });
 
-myChinmei.on('error', (err) => {
-	console.error(err);
-});
+/* user should be equal to something like this:
+
+{
+  id: '6338595',
+  username: '_julien'
+}
+*/
